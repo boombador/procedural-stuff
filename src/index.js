289ba49484e33b38696ccda81c291e260748dad5
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2017-06-13 16:13:09 -0400

    Rely on index.html plugin

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import render from './procedural.js';
 require('ace-css/css/ace.css');
 require('font-awesome/css/font-awesome.css');
 
-// Require index.html so it gets copied to dist
-require('./index.html');
-
-console.log('hello');
+render();
 
 //var Elm = require('./Main.elm');
 //var mountNode = document.getElementById('main');
