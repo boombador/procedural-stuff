d6a3b7dbc861db26075ee90266b576bf5dce2c78
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2017-06-14 19:23:42 -0400

    start on rendering more complex terrain

--- elm-src/Procedural.elm
+++ elm-src/Procedural.elm
@@ -3,13 +3,17 @@ module Procedural exposing (..)
 import Math.Vector3 as Vec3 exposing (vec3, Vec3)
 
 
+type alias TriangleMesh =
+    List ( Vertex, Vertex, Vertex )
+
+
 type alias Vertex =
     { position : Vec3
     , color : Vec3
     }
 
 
-tri : Vec3 -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
+tri : Vec3 -> Vec3 -> Vec3 -> TriangleMesh
 tri x y z =
     [ ( Vertex x (vec3 1 0 0)
       , Vertex y (vec3 0 1 0)
@@ -18,7 +22,7 @@ tri x y z =
     ]
 
 
-quad : Vec3 -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
+quad : Vec3 -> Vec3 -> Vec3 -> TriangleMesh
 quad corner x y =
     let
         ( a, b, c, d ) =
@@ -34,12 +38,12 @@ quad corner x y =
             ]
 
 
-cube : Float -> Vec3 -> List ( Vertex, Vertex, Vertex )
+cube : Float -> Vec3 -> TriangleMesh
 cube s =
     prism s s s
 
 
-prism : Float -> Float -> Float -> Vec3 -> List ( Vertex, Vertex, Vertex )
+prism : Float -> Float -> Float -> Vec3 -> TriangleMesh
 prism w h l center =
     let
         ( x, y, z ) =
@@ -70,14 +74,14 @@ prism w h l center =
             ]
 
 
-posts : List Vec3 -> List ( Vertex, Vertex, Vertex )
+posts : List Vec3 -> TriangleMesh
 posts locations =
     locations
         |> List.map post
         |> List.concat
 
 
-postsBetweenEndpoints : Int -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
+postsBetweenEndpoints : Int -> Vec3 -> Vec3 -> TriangleMesh
 postsBetweenEndpoints count a b =
     let
         distance =
@@ -130,14 +134,14 @@ intermediatePoints targetSplit a b =
             |> List.map fromS
 
 
-postsBetweenPoints : ( Vec3, Vec3 ) -> List ( Vertex, Vertex, Vertex )
+postsBetweenPoints : ( Vec3, Vec3 ) -> TriangleMesh
 postsBetweenPoints ( a, b ) =
     List.concat
         [ posts (intermediatePoints 0.5 a b)
         ]
 
 
-postsFromPath : List Vec3 -> List ( Vertex, Vertex, Vertex )
+postsFromPath : List Vec3 -> TriangleMesh
 postsFromPath points =
     let
         endpointPairs =
@@ -153,7 +157,7 @@ postsFromPath points =
             ]
 
 
-roof : Float -> Float -> Float -> Float -> List ( Vertex, Vertex, Vertex )
+roof : Float -> Float -> Float -> Float -> TriangleMesh
 roof hw hl roofTop roofBase =
     let
         ( roofA, roofB, cornerA, cornerB ) =
@@ -175,7 +179,7 @@ roof hw hl roofTop roofBase =
             ]
 
 
-roofSides : Float -> Float -> Float -> Float -> List ( Vertex, Vertex, Vertex )
+roofSides : Float -> Float -> Float -> Float -> TriangleMesh
 roofSides hw hl roofBase roofTop =
     List.concat
         [ tri (vec3 hw roofBase hl) (vec3 hw roofBase -hl) (vec3 hw roofTop 0)
@@ -183,7 +187,7 @@ roofSides hw hl roofBase roofTop =
         ]
 
 
-post : Vec3 -> List ( Vertex, Vertex, Vertex )
+post : Vec3 -> TriangleMesh
 post base =
     let
         ( x, y, z ) =
@@ -242,7 +246,7 @@ houseOpts width height length roofHeight start =
         }
 
 
-sceneTriangles : List ( Vertex, Vertex, Vertex )
+sceneTriangles : TriangleMesh
 sceneTriangles =
     let
         ( w, h, l, r, origin ) =
@@ -251,11 +255,13 @@ sceneTriangles =
         List.concat
             [ house w h l r origin
             , postsFromPath (rectangularPath w l)
-            , terrain 10
+            , groundPlane 10
+
+            --, terrainToMesh (terrain 2 2 3 3 [ 0, 0, 0, 0 ])
             ]
 
 
-house : Float -> Float -> Float -> Float -> Vec3 -> List ( Vertex, Vertex, Vertex )
+house : Float -> Float -> Float -> Float -> Vec3 -> TriangleMesh
 house h w l r start =
     let
         { width, height, length, roofHeight, hw, hh, hl, center, roofBase, roofTop } =
@@ -268,18 +274,63 @@ house h w l r start =
             ]
 
 
-terrain : Float -> List ( Vertex, Vertex, Vertex )
-terrain toEdge =
+groundPlane : Float -> TriangleMesh
+groundPlane toEdge =
     let
-        ( x, z ) =
+        ( x, z, offset ) =
             ( Vec3.scale toEdge Vec3.i
             , Vec3.scale toEdge Vec3.k
+            , -(toEdge / 2)
             )
-
-        offset =
-            -(toEdge / 2)
     in
         quad (vec3 offset 0 offset) x z
+
+
+type alias Terrain =
+    ( Int, Int, Float, Float, List Float )
+
+
+terrain : Int -> Int -> Float -> Float -> List Float -> Terrain
+terrain xSegments zSegments xDelta zDelta heightList =
+    ( xSegments, zSegments, xDelta, zDelta, heightList )
+
+
+{-| This is making the plane go over in the wrong area, but otherwise is
+looking good
+-}
+terrainToMesh : Terrain -> TriangleMesh
+terrainToMesh t =
+    let
+        ( x, z, dx, dz, heights ) =
+            t
+
+        withIndices : Int -> Float -> ( Int, Int, Float )
+        withIndices idx h =
+            ( rem idx x, (//) idx z, h )
+
+        quadFunc : ( Int, Int, Float ) -> TriangleMesh
+        quadFunc =
+            toQuad dx dz
+    in
+        heights
+            |> List.indexedMap withIndices
+            |> List.map quadFunc
+            |> List.concat
+
+
+toQuad : Float -> Float -> ( Int, Int, Float ) -> TriangleMesh
+toQuad dx dz triplet =
+    let
+        ( ix, iz, h ) =
+            triplet
+
+        ( xSide, zSide ) =
+            ( toFloat ix * dx, toFloat iz * dz )
+
+        start =
+            vec3 xSide h zSide
+    in
+        quad start (Vec3.scale xSide Vec3.i) (Vec3.scale zSide Vec3.k)
 
 
 toOffsetList : List Vec3 -> List Vec3
